1f6228a4b8c5507d17e2c00ae245f7c984d8b34a
Ples Alex <plesalex100@gmail.com>
2020-10-08 15:48:11 +0300

    Moore's Law

---
+++ Moores Law/Moores Law/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Moores_Law
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N = ?");
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                double months = Math.Log(n, 2) * 18;
+                Console.WriteLine($"Vom avea procesoare de {n} ori mai puternice la acelasi pret in {Math.Floor(months / 12)} ani si {Math.Floor(months % 12)} luni");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
