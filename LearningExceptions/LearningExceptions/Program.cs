fba75ec27b332c70cc8035fc96e140dd1694532e
Ples Alex <plesalex100@gmail.com>
2020-10-12 17:40:26 +0300

    Learning using exceptions

---
+++ LearningExceptions/LearningExceptions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LearningExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Introduceti un numar Natural format din 2 cifre");
+            string line = Console.ReadLine();
+            
+            try
+            {
+                int number;
+                if(!int.TryParse(line, out number))
+                {
+                    throw new Exception("Nu ati introdus un numar");
+                }
+
+                if(number < 10 || number > 99)
+                {
+                    throw new Exception("Numarul nu este format din 2 cifre !");
+                }
+
+                Console.WriteLine($"Numarul este {number} si este format din 2 cifre");
+
+            } catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
